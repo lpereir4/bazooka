09d31108ce797e4efb7db1cbbca3733b6fb3a605
Julien Vey <vey.julien@gmail.com>
2015-03-02 13:26:39 +0100

    trim gopath

--- scripts/push-images.sh
+++ scripts/push-images.sh
@@ -5,9 +5,11 @@ set -e
 docker_projects=( "parser" "parserlang/golang" "parserlang/java" "parserlang/python" "parserlang/nodejs" "orchestration" \
 "server" "web")
 
+gopath_arr=$(echo $GOPATH | tr ":" "\n")
+
 for project in "${docker_projects[@]}"
 do
-  pushd "$GOPATH/src/github.com/bazooka-ci/bazooka/$project"
+  pushd "${gopath_arr[0]}/src/github.com/bazooka-ci/bazooka/$project"
   make push
   popd
 done
